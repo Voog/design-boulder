d138884bdfb5ef18c679718c4043b7959333d905
Kaspar Naaber <kaspar@fraktal.ee>
2013-12-03 15:13:33 +0200

    Add common page basic layout

--- products.php
+++ products.php
@@ -12,26 +12,28 @@
 			<!-- {% include "site-header" %} -->
 
 		<main class="page-content cfx" role="main">
-			<header class="content-header">
-				<h1>This is where your products live.</h1>
-				<!-- <h1>{% contentblock name="slogan" %}{{ "title_goes_here" | lc }}{% endcontentblock %}</h1> -->
-			</header>
-			<h2>INTRODUCTION</h2>
-			<p>Introduce your products by writing text, showing pictures, building forms, adding galleries. If you don't need a Products page you can rename or delete it from the page properties tool on the lower right corner.</p>
-			<h3>This is an example unordered list</h3>
-			<ul>
-				<li>Item one</li>
-				<li>Another item</li>
-				<li>Third one</li>
-			</ul>
-			<h3>This is an example ordered list</h3>
-			<ol>
-				<li>Item one</li>
-				<li>Another item</li>
-				<li>Third one</li>
-			</ol>
-			If you require a more thorough product database or e-commerce, then we've got some great custom solutions for you. Contact Edicy support and let's get it done!
-				<!-- {% content %} -->
+			<div class="content-container">
+				<header class="content-header">
+					<h1>This is where your products live.</h1>
+					<!-- <h1>{% contentblock name="slogan" %}{{ "title_goes_here" | lc }}{% endcontentblock %}</h1> -->
+				</header>
+				<h2>INTRODUCTION</h2>
+				<p>Introduce your products by writing text, showing pictures, building forms, adding galleries. If you don't need a Products page you can rename or delete it from the page properties tool on the lower right corner.</p>
+				<h3>This is an example unordered list</h3>
+				<ul>
+					<li>Item one</li>
+					<li>Another item</li>
+					<li>Third one</li>
+				</ul>
+				<h3>This is an example ordered list</h3>
+				<ol>
+					<li>Item one</li>
+					<li>Another item</li>
+					<li>Third one</li>
+				</ol>
+				If you require a more thorough product database or e-commerce, then we've got some great custom solutions for you. Contact Edicy support and let's get it done!
+					<!-- {% content %} -->
+			</div>
 		</main>
 
 		<?php require("components/site-footer.php") ?>
